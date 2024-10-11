9db8340789c5ac1c1ce76ce07121012aceffe19e
Tearp89 <marla838.aguilar@gmail.com>
2024-10-11 11:43:40 -0600

    Estructura del servicio

---
+++ ServidorBMC/ServiceBMC/Validators/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceBMC.Validators
+{
+    internal class Class1
+    {
+    }
+}
